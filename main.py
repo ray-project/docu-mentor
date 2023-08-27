04a70140be6a58ad10988e68dbfefd16dcea4c29
Max Pumperla <max.pumperla@googlemail.com>
2023-08-27 15:04:26 +0200

    more functionality
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- main.py
+++ main.py
@@ -10,10 +10,43 @@ GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
 app = FastAPI()
 
+HEADERS = {
+    "Authorization": f"token {GITHUB_TOKEN}",
+    "User-Agent": "GitHub-PR-Bot",
+    # "Accept": "application/vnd.github.v3+json"
+}
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+async def get_pr_files(owner, repo, pr_number):
+    url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}/files"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=HEADERS)
+        return response.json()
+
+
+async def get_pr_diff(owner, repo, pr_number):
+    url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=HEADERS)
+        diff_url = response.json().get('diff_url')
+        
+        diff_response = await client.get(diff_url, headers=HEADERS)
+        return diff_response.text
+
+
+async def post_pr_comment(owner, repo, pr_number, comment_body):
+    url = f"https://api.github.com/repos/{owner}/{repo}/issues/{pr_number}/comments"
+    data = {
+        "body": comment_body
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=data, headers=HEADERS)
+        return response.json()
 
 
 @app.post("/webhook/")
@@ -25,23 +58,16 @@ async def handle_github_webhook(request: Request):
     if pr and (data["action"] in ["opened", "synchronized"]):
         async with httpx.AsyncClient() as client:
             # Fetch diff from GitHub
-            resp = await client.get(pr["diff_url"], headers={
-                "Authorization": f"token {GITHUB_TOKEN}",
-                "User-Agent": "GitHub-Diff-Bot"
-            })
+            resp = await client.get(pr["diff_url"], headers=HEADERS)
             
             diff = resp.text
+            print(diff)
             
-            # Analyze the diff (this is a simplistic check)
-            if "TODO" in diff:
-                # Found a TODO in the PR, let's comment on the PR
-                await client.post(
-                    f"{pr['issue_url']}/comments",
-                    json={"body": "Found a TODO in your PR!"},
-                    headers={
-                        "Authorization": f"token {GITHUB_TOKEN}",
-                        "User-Agent": "GitHub-Diff-Bot"
-                    }
-                )
+            # Let's comment on the PR
+            await client.post(
+                f"{pr['issue_url']}/comments",
+                json={"body": "Found your PR!"},
+                headers=HEADERS
+            )
     
     return {"status": "success"}
