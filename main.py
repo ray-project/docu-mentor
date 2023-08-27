e554bf567eabc1eb54ccc4537b8e3490fb1f7e72
Max Pumperla <max.pumperla@googlemail.com>
2023-08-27 16:26:31 +0200

    correct diff url
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 HEADERS = {
     "Authorization": f"token {GITHUB_TOKEN}",
     "User-Agent": "GitHub-PR-Bot",
-    # "Accept": "application/vnd.github.v3+json"
+    "Accept": "application/vnd.github.v3+json"
 }
 
 
@@ -41,12 +41,10 @@ async def get_pr_files(owner, repo, pr_number):
 
 
 async def get_pr_diff(owner, repo, pr_number):
-    url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}"
+    url = f"https://patch-diff.githubusercontent.com/raw/{owner}/{repo}/pull/{pr_number}.diff"
+
     async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=HEADERS)
-        diff_url = response.json().get('diff_url')
-        
-        diff_response = await client.get(diff_url, headers=HEADERS)
+        diff_response = await client.get(url, headers=HEADERS)
         return diff_response.text
 
 
@@ -70,9 +68,13 @@ async def handle_github_webhook(request: Request):
     if pr and (data["action"] in ["opened", "synchronize"]):
         async with httpx.AsyncClient() as client:
             # Fetch diff from GitHub
-            resp = await client.get(pr["diff_url"], headers=HEADERS)
-            
-            diff = resp.text
+            url = pr.get("url")
+            parts = url.split("/")
+            owner, repo, pr_number = parts[-4], parts[-3], parts[-2]
+            url = f"https://patch-diff.githubusercontent.com/raw/{owner}/{repo}/pull/{pr_number}.diff"
+
+            diff_response = await client.get(url, headers=HEADERS)
+            diff = diff_response.text
             logger.info("Raw diff with meta-data:" + diff)
 
             files_with_diff = {}
@@ -83,6 +85,7 @@ async def handle_github_webhook(request: Request):
                     files_with_diff[current_file] = {'text': []}
                 elif line.startswith('+') and not line.startswith('+++'):
                     files_with_diff[current_file]['text'].append(line[1:])
+            logger.info(files_with_diff)
 
             chat_completion = openai.ChatCompletion.create(
                 model="meta-llama/Llama-2-70b-chat-hf",
