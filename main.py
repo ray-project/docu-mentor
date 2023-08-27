0f0241af958f7861321aa10e507f4d125596d2bb
Max Pumperla <max.pumperla@googlemail.com>
2023-08-27 15:59:48 +0200

    update bot
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- main.py
+++ main.py
@@ -75,18 +75,25 @@ async def handle_github_webhook(request: Request):
             diff = resp.text
             logger.info("Raw diff with meta-data:" + diff)
 
-            additions = []
+            files_with_diff = {}
+            current_file = None
             for line in diff.split('\n'):
-                if line.startswith('+') and not line.startswith('+++'):
-                    additions.append(line[1:])
-            logger.info("This is the diff:" + diff)
+                if line.startswith('diff --git'):
+                    current_file = line.split(' ')[2][2:]
+                    files_with_diff[current_file] = {'text': []}
+                elif line.startswith('+') and not line.startswith('+++'):
+                    files_with_diff[current_file]['text'].append(line[1:])
+
+            logger.info("This is the extracted diff:" + files_with_diff)
 
             chat_completion = openai.ChatCompletion.create(
                 model="meta-llama/Llama-2-70b-chat-hf",
                 messages=[
                      {"role": "system", "content": "You are a helpful assistant." +
-                       "Improve the following content. Criticise grammar, punctuation, style etc." +
-                       "Make it so that you recommend common technical writing knowledge"}, 
+                       "Improve the following <content>. Criticise grammar, punctuation, style etc." +
+                       "Make it so that you recommend common technical writing knowledge " +
+                       "The <content> will be in JSON format and contain file names and 'text'." +
+                       "Make sure to give concrete feedback per file."}, 
                      {"role": "user", "content": f"This is the content: {diff}"}],
                 temperature=0.7
             )
