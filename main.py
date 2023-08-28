44c64775c01dbbfec1e2ab39de8a50c11d7d696f
Max Pumperla <max.pumperla@googlemail.com>
2023-08-28 17:08:03 +0200

    full bot
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- main.py
+++ main.py
@@ -15,7 +15,23 @@ logger = logging.getLogger("Doc Sanity")
 
 load_dotenv()
 
-greeting = "👋 Hi, I'm @doc-sanity, an LLM-powered GitHub app that gives you actionable feedback on the writing contained in your pull requests."
+GREETING = """
+👋 Hi, I'm @doc-sanity, an LLM-powered GitHub app 
+that gives you actionable feedback on your writing.
+
+Simply create a new comment in this PR that says:
+
+@doc-sanity run
+
+and I will start my analysis. I only look at what you changed
+in this PR. If you only want me to look at specific files or folders,
+you can specify them like this:
+
+@doc-sanity run doc/ README.md
+
+In this example, I'll have a look at all files contained in the "doc/"
+folder and the file "README.md". All good? Let's get started!
+"""
 
 openai.api_base = "https://api.endpoints.anyscale.com/v1"
 openai.api_key = os.environ.get("OPENAI_API_KEY")
@@ -81,7 +97,7 @@ def files_to_diff_dict(diff):
 @app.post("/webhook/")
 async def handle_github_webhook(request: Request):
     data = await request.json()
-    logger.info(data)
+    # logger.info(data)
 
     headers = {
         "Authorization": f"token {GITHUB_TOKEN}",
@@ -107,75 +123,95 @@ async def handle_github_webhook(request: Request):
             'Accept': 'application/vnd.github.VERSION.diff'
         }    
     
-    # Check if the event is a issue comment
+    # Check if the event is a new or modified issue comment
     issue = data["issue"]
     if issue and data.get("action") in ["created", "edited"]:
 
         # Check if the issue is a pull request
         if "/pull/" in issue["html_url"]:
+
+            pr = issue.get("pull_request")
             
             # Get the comment body
             comment = data.get("comment")
             comment_body = comment.get("body")
 
             # Check if the bot is mentioned in the comment
-            if "@doc-sanity" in comment_body:
+            if "@doc-sanity help" in comment_body:
                 # The bot is mentioned in the PR comment
                 async with httpx.AsyncClient() as client:
                     await client.post(
                         f"{comment['issue_url']}/comments",
                         json={
-                            "body": f"I react to @mentions!"
+                            "body": GREETING
+                        },
+                        headers=headers
+                    )
+            elif "@doc-sanity run" in comment_body:
+                async with httpx.AsyncClient() as client:
+                    # Fetch diff from GitHub
+
+                    files_to_keep = comment_body.replace("doc-sanity run ", "").split(" ")
+
+                    url = get_diff_url(pr)
+                    diff_response = await client.get(url, headers=headers)
+                    diff = diff_response.text
+                    logger.info("Raw diff with meta-data:" + diff)
+
+                    files_with_diff = files_to_diff_dict(diff)
+                    keys_to_keep = ['app']
+
+                    # Filter the dictionary
+                    if files_to_keep:
+                        files_with_diff = {
+                            k: files_with_diff[k] for k in files_with_diff if any(sub in k for sub in keys_to_keep)
+                        }
+
+                    logger.info(files_with_diff)
+
+                    chat_completion = openai.ChatCompletion.create(
+                        model="meta-llama/Llama-2-70b-chat-hf",
+                        messages=[
+                            {"role": "system", 
+                            "content": "You are a helpful assistant." +
+                            "Improve the following <content>. Criticise grammar, punctuation, style etc." +
+                            "Make it so that you recommend common technical writing knowledge " +
+                            "The <content> will be in JSON format and contain file names and 'text'. " +
+                            "You can use GitHub-flavored markdown syntax. " +
+                            "Make sure to give very concise feedback per file."}, 
+                            {"role": "user", 
+                            "content": f"This is the content: {files_with_diff}"}
+                        ],
+                        temperature=0.7
+                    )
+
+                    logger.info(chat_completion)
+                    model = chat_completion.get("model")
+                    usage = chat_completion.get("usage")
+                    prompt_tokens = usage.get("prompt_tokens")
+                    completion_tokens = usage.get("completion_tokens")
+                    content = chat_completion["choices"][0]["message"]["content"]
+                                
+                    # Let's comment on the PR
+                    await client.post(
+                        f"{pr['issue_url']}/comments",
+                        json={
+                            "body": f":rocket: Doc Sanity finished analysing your PR! :rocket:\n\n" +
+                            "Take a look at your results:\n" +
+                            f"{content}\n\n" +
+                            "This bot is proudly powered by [Anyscale Endpoints](https://app.endpoints.anyscale.com/).\n" +
+                            f"It used the model {model}, used {prompt_tokens} prompt tokens, and {completion_tokens} completion tokens in total."
                         },
                         headers=headers
                     )
     
     # Ensure PR exists and is opened or synchronized
-    pr = data.get("pull_request")
-    if pr and (data["action"] in ["opened", "synchronize"]):
+    if data.get("pull_request") and (data["action"] in ["opened"]): # use "synchronize" for tracking commits
         async with httpx.AsyncClient() as client:
-            # Fetch diff from GitHub
-
-            url = get_diff_url(pr)
-            diff_response = await client.get(url, headers=headers)
-            diff = diff_response.text
-            logger.info("Raw diff with meta-data:" + diff)
-
-            files_with_diff = files_to_diff_dict(diff)
-            logger.info(files_with_diff)
-
-            chat_completion = openai.ChatCompletion.create(
-                model="meta-llama/Llama-2-70b-chat-hf",
-                messages=[
-                    {"role": "system", 
-                     "content": "You are a helpful assistant." +
-                     "Improve the following <content>. Criticise grammar, punctuation, style etc." +
-                     "Make it so that you recommend common technical writing knowledge " +
-                     "The <content> will be in JSON format and contain file names and 'text'. " +
-                     "You can use GitHub-flavored markdown syntax. " +
-                     "Make sure to give very concise feedback per file."}, 
-                    {"role": "user", 
-                     "content": f"This is the content: {files_with_diff}"}
-                ],
-                temperature=0.7
-            )
-
-            logger.info(chat_completion)
-            model = chat_completion.get("model")
-            usage = chat_completion.get("usage")
-            prompt_tokens = usage.get("prompt_tokens")
-            completion_tokens = usage.get("completion_tokens")
-            content = chat_completion["choices"][0]["message"]["content"]
-                        
-            # Let's comment on the PR
             await client.post(
-                f"{pr['issue_url']}/comments",
+                f"{comment['issue_url']}/comments",
                 json={
-                    "body": f":rocket: Doc Sanity finished analysing your PR! :rocket:\n\n" +
-                    "Take a look at your results:\n" +
-                    f"{content}\n\n" +
-                    "This bot is proudly powered by [Anyscale Endpoints](https://app.endpoints.anyscale.com/).\n" +
-                    f"It used the model {model}, used {prompt_tokens} prompt tokens, and {completion_tokens} completion tokens in total."
+                    "body": GREETING
                 },
                 headers=headers
             )
