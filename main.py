96866e4b3425ff3877097b076ccbbf026ce2e120
Max Pumperla <max.pumperla@googlemail.com>
2023-09-08 10:02:03 +0200

    refactor
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from utils import (
 
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logger = logging.getLogger("Doc Sanity")
+logger = logging.getLogger("Docu Mentor")
 
 
 GREETING = """
@@ -85,123 +85,129 @@ def sanitize(
 
 app = FastAPI()
 
+async def smoke_test():
+    return  {"message": "Docu Mentor reporting for duty!"}
+
+async def handle_webhook(request: Request):
+    data = await request.json()
+
+    installation = data.get("installation")
+    if installation and installation.get("id"):
+        installation_id = installation.get("id")
+        logger.info(f"Installation ID: {installation_id}")
+
+        JWT_TOKEN = generate_jwt()
+
+        installation_access_token = await get_installation_access_token(
+            JWT_TOKEN, installation_id
+        )
+
+        headers = {
+            "Authorization": f"token {installation_access_token}",
+            "User-Agent": "Your-App-Name",
+            "Accept": "application/vnd.github.VERSION.diff",
+        }
+    else:
+        raise ValueError("No app installation found.")
+
+    # If PR exists and is opened
+    if "pull_request" in data.keys() and (
+        data["action"] in ["opened"]
+    ):  # use "synchronize" for tracking new commits
+        pr = data.get("pull_request")
+
+        async with httpx.AsyncClient() as client:
+            await client.post(
+                f"{pr['issue_url']}/comments",
+                json={"body": GREETING},
+                headers=headers,
+            )
+
+        return JSONResponse(content={}, status_code=200)
+
+    # Check if the event is a new or modified issue comment
+    if "issue" in data.keys() and data.get("action") in ["created", "edited"]:
+        issue = data["issue"]
+
+        # Check if the issue is a pull request
+        if "/pull/" in issue["html_url"]:
+            pr = issue.get("pull_request")
+
+            # Get the comment body
+            comment = data.get("comment")
+            comment_body = comment.get("body")
+            # Remove all whitespace characters except for regular spaces
+            comment_body = comment_body.translate(
+                str.maketrans("", "", string.whitespace.replace(" ", ""))
+            )
+
+            # Skip if the bot talks about itself
+            author_handle = comment["user"]["login"]
+
+            # Check if the bot is mentioned in the comment
+            if (
+                author_handle != "docu-mentor[bot]"
+                and "@docu-mentor run" in comment_body
+            ):
+                async with httpx.AsyncClient() as client:
+                    # Fetch diff from GitHub
+                    files_to_keep = comment_body.replace(
+                        "@docu-mentor run", ""
+                    ).split(" ")
+                    files_to_keep = [item for item in files_to_keep if item]
+
+                    logger.info(files_to_keep)
+
+                    url = get_diff_url(pr)
+                    diff_response = await client.get(url, headers=headers)
+                    diff = diff_response.text
+
+                    files_with_diff = files_to_diff_dict(diff)
+
+                    # Filter the dictionary
+                    if files_to_keep:
+                        files_with_diff = {
+                            k: files_with_diff[k]
+                            for k in files_with_diff
+                            if any(sub in k for sub in files_to_keep)
+                        }
+
+                    logger.info(files_with_diff.keys())
+
+                    # Sanitize the content
+                    chat_completion = sanitize(files_with_diff)
+
+                    logger.info(chat_completion)
+                    model = chat_completion.get("model")
+                    usage = chat_completion.get("usage")
+                    prompt_tokens = usage.get("prompt_tokens")
+                    completion_tokens = usage.get("completion_tokens")
+                    content = chat_completion["choices"][0]["message"]["content"]
+
+                    # Let's comment on the PR
+                    await client.post(
+                        f"{comment['issue_url']}/comments",
+                        json={
+                            "body": f":rocket: Docu Mentor finished analysing your PR! :rocket:\n\n"
+                            + "Take a look at your results:\n"
+                            + f"{content}\n\n"
+                            + "This bot is proudly powered by [Anyscale Endpoints](https://app.endpoints.anyscale.com/).\n"
+                            + f"It used the model {model}, used {prompt_tokens} prompt tokens, "
+                            + f"and {completion_tokens} completion tokens in total."
+                        },
+                        headers=headers,
+                    )
+
 @serve.deployment(route_prefix="/")
 @serve.ingress(app)
 class ServeBot:
     @app.get("/")
     async def root(self):
-        return {"message": "Doc Sanity reporting for duty!"}
+        return await smoke_test()
 
     @app.post("/webhook/")
-    async def handle_github_webhook(self, request: Request):
-        data = await request.json()
-
-        installation = data.get("installation")
-        if installation and installation.get("id"):
-            installation_id = installation.get("id")
-            logger.info(f"Installation ID: {installation_id}")
-
-            JWT_TOKEN = generate_jwt()
-
-            installation_access_token = await get_installation_access_token(
-                JWT_TOKEN, installation_id
-            )
-
-            headers = {
-                "Authorization": f"token {installation_access_token}",
-                "User-Agent": "Your-App-Name",
-                "Accept": "application/vnd.github.VERSION.diff",
-            }
-        else:
-            raise ValueError("No app installation found.")
-
-        # If PR exists and is opened
-        if "pull_request" in data.keys() and (
-            data["action"] in ["opened"]
-        ):  # use "synchronize" for tracking new commits
-            pr = data.get("pull_request")
-
-            async with httpx.AsyncClient() as client:
-                await client.post(
-                    f"{pr['issue_url']}/comments",
-                    json={"body": GREETING},
-                    headers=headers,
-                )
-
-            return JSONResponse(content={}, status_code=200)
-
-        # Check if the event is a new or modified issue comment
-        if "issue" in data.keys() and data.get("action") in ["created", "edited"]:
-            issue = data["issue"]
-
-            # Check if the issue is a pull request
-            if "/pull/" in issue["html_url"]:
-                pr = issue.get("pull_request")
-
-                # Get the comment body
-                comment = data.get("comment")
-                comment_body = comment.get("body")
-                # Remove all whitespace characters except for regular spaces
-                comment_body = comment_body.translate(
-                    str.maketrans("", "", string.whitespace.replace(" ", ""))
-                )
-
-                # Skip if the bot talks about itself
-                author_handle = comment["user"]["login"]
-
-                # Check if the bot is mentioned in the comment
-                if (
-                    author_handle != "docu-mentor[bot]"
-                    and "@docu-mentor run" in comment_body
-                ):
-                    async with httpx.AsyncClient() as client:
-                        # Fetch diff from GitHub
-                        files_to_keep = comment_body.replace(
-                            "@docu-mentor run", ""
-                        ).split(" ")
-                        files_to_keep = [item for item in files_to_keep if item]
-
-                        logger.info(files_to_keep)
-
-                        url = get_diff_url(pr)
-                        diff_response = await client.get(url, headers=headers)
-                        diff = diff_response.text
-
-                        files_with_diff = files_to_diff_dict(diff)
-
-                        # Filter the dictionary
-                        if files_to_keep:
-                            files_with_diff = {
-                                k: files_with_diff[k]
-                                for k in files_with_diff
-                                if any(sub in k for sub in files_to_keep)
-                            }
-
-                        logger.info(files_with_diff.keys())
-
-                        # Sanitize the content
-                        chat_completion = sanitize(files_with_diff)
-
-                        logger.info(chat_completion)
-                        model = chat_completion.get("model")
-                        usage = chat_completion.get("usage")
-                        prompt_tokens = usage.get("prompt_tokens")
-                        completion_tokens = usage.get("completion_tokens")
-                        content = chat_completion["choices"][0]["message"]["content"]
-
-                        # Let's comment on the PR
-                        await client.post(
-                            f"{comment['issue_url']}/comments",
-                            json={
-                                "body": f":rocket: Doc Sanity finished analysing your PR! :rocket:\n\n"
-                                + "Take a look at your results:\n"
-                                + f"{content}\n\n"
-                                + "This bot is proudly powered by [Anyscale Endpoints](https://app.endpoints.anyscale.com/).\n"
-                                + f"It used the model {model}, used {prompt_tokens} prompt tokens, "
-                                + f"and {completion_tokens} completion tokens in total."
-                            },
-                            headers=headers,
-                        )
+    async def handle_webhook_route(self, request: Request):
+        return await handle_webhook(request)
 
 
 # Run with: serve run main:bot
